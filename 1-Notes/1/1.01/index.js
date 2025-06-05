9fcfd90a6f2a549accf5ee8ec4043a75f353ca96
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2025-06-05 11:43:40 +0000

    SOME CHANGES

--- 1-Notes/1/1.01/index.js
+++ 1-Notes/1/1.01/index.js
@@ -4,12 +4,12 @@ const express = require("express")
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send("hi")
 })
 
-app.listen(process.env.PORT,()=>[
-    console.log('app listening at port :',process.env.PORT)
+app.listen(PORT,()=>[
+    console.log('app listening at port :',PORT)
 ])
