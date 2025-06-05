b7c0fd1f4586101b85224187497a70607feab2e2
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2025-06-05 12:17:50 +0000

    added axios

--- 1-Notes/1/1.02/frontend/src/App.jsx
+++ 1-Notes/1/1.02/frontend/src/App.jsx
@@ -2,9 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const[jokes,setJokes] = useState([])
   return (
     <> 
     <h1>Frontend intialized</h1>
+    <p>
+      JOKES: {jokes.length}
+    </p>
+    {
+      jokes.map((joke,index)=>{
+        <div key={joke.index}>
+          <h3>{joke.title}</h3>
+          <p>{joke.joke}</p>
+        </div>
+      })
+    }
     </>
   )
 }
